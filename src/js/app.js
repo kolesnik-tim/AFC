a9d0e5d2e9a8a295049e94bed06cfcd70de139d2
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-01-23 16:49:33 +0200

    !

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 import './import/preloader.js';
 import './import/slider.js';
 import './import/parallax.js';
+
+// onclick="$('#bottom').slideDown(); $('.up').css({display: 'none'})
+
+$('.up').on('click', function() {
+  $('#bottom').slideDown();
+  $(this).slideUp();
+});
